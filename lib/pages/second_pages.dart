fd573cb392bbaa9eeb23309b08f57af79aa5a743
FlutLab.io <>
2021-05-27 17:56:59 +0000

    Autosave

--- lib/pages/second_pages.dart
+++ lib/pages/second_pages.dart
@@ -42,24 +42,49 @@ class SecondPage extends StatelessWidget {
                   height: 40,
                 ),
                 TextField(
-                  decoration: InputDecoration(hintText: "mensaje", icon: Icon(Icons.email)), //fin de inputdecoration
+                  decoration: InputDecoration(hintText: "Mensaje", icon: Icon(Icons.email)), //fin de inputdecoration
                 ), //fin de textfield
                 SizedBox(
                   height: 40,
                 ),
-                TextField(
-                  decoration: InputDecoration(hintText: "Telefono", icon: Icon(Icons.vpn_key)), //fin de inputdecoration
-                ), //fin de textfield
-                SizedBox(
-                  height: 40,
-                ),
+              
                 TextField(
                   decoration: InputDecoration(hintText: "Sucursal", icon: Icon(Icons.vpn_key)), //fin de inputdecoration
                 ), //fin de textfield
                 SizedBox(
                   height: 40,
                 ),
-               
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    FlatButton(
+                      child: Text('Enviar'),
+                      color: Colors.amber,
+                      // borderRadius: BorderRadius.circular(30.0),
+                      padding: EdgeInsets.all(20.0),
+                      onPressed: () {},
+                    ), //Fin FlatButton
+                    new Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    ),
+
+                    RaisedButton(
+                      child: Text(
+                        'CANCELAR',
+                      ),
+                      color: Colors.red[600],
+                      // borderRadius: BorderRadius.circular(30.0),
+                      splashColor: Colors.cyan,
+                      animationDuration: Duration(seconds: 20),
+                      padding: EdgeInsets.all(20.0),
+                      colorBrightness: Brightness.light,
+                      onPressed: () {},
+                    ), //Fin Raised Button
+                    SizedBox(
+                      height: 40,
+                    ),
+                  ], //Fin niños Widget
+                ), //Fin de Niño Row
               ], //fin de widget
             ), //fin de column
           ], //fin de widget
